f2947907bb80694701dde105266bee5836d331ed
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-10-22 18:19:46 -0700

    Imperative programming for two more extra credit problems

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -116,9 +116,7 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
   
-  it("should find the largest prime factor of a composite number", function () {
-
-    function isPrime(n){
+  function isPrime(n){
       
       if(n<=1){
         return false;
@@ -132,6 +130,8 @@ describe("About Applying What We Have Learnt", function() {
 
       return true;
     }
+
+  it("should find the largest prime factor of a composite number", function () {
 
     var prime = function largestPrime(num){
           if(isPrime(num)) { return "Please Enter a composite number first!!"};
@@ -152,19 +152,62 @@ describe("About Applying What We Have Learnt", function() {
   /*
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
     
-  });
+  }); */
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
       
-    
-  });
+    function smallestDivisibleNumber(n){
 
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
+      smallestNumb = n+1;
+      flag = true;
+
+      while (true) {
+        
+        for (var i=1; i<=n; i++){
+          if (smallestNumb%i !== 0){
+            flag = false;
+            break;
+          }
+        } 
+        
+        if(flag){
+          return smallestNumb;
+        }
+        else {
+          smallestNumb += 1 ;
+          flag = true;
+        }
+      
+      }
+      
+    }
     
-  });
+    expect(smallestDivisibleNumber(20)).toBe(232792560);
+
+  }); 
+
+/*  it("should find the difference between the sum of the squares and the square of the sums", function () {
+    
+  });  */
 
   it("should find the 10001st prime", function () {
-
-  });
-  */
+      
+      function nthPrime(n){
+        var i = 2;
+        var count = 0;
+        while(true) {
+          if (isPrime(i)){
+            count += 1;
+            if (count === n) {
+              return i;
+            }
+          }
+          i = i+1 ;
+        }
+      }
+     
+    expect(nthPrime(10001)).toBe(104743);
+    
+  }); 
+  
 });
